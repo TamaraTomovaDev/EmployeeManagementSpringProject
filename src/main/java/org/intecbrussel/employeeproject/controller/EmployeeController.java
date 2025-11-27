7b1cae57dfc554057e0cf75b56ba53ce4aa89560
Tamara <toma_tomova@hotmail.com>
2025-11-27 13:44:26 +0100

    Employee Management Spring Project

--- src/main/java/org/intecbrussel/employeeproject/controller/EmployeeController.java
+++ src/main/java/org/intecbrussel/employeeproject/controller/EmployeeController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/employees")
+@RequestMapping("/employees")
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
